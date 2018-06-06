4af5e5922c9fd013b7d8ac12c258e19dcedc739c
Gokulakrishna <gokulakrishnaks@gmail.com>
2018-06-06 13:08:36 +0000

    Added deploy.js

---
+++ deploy.js
@@ -0,0 +1,58 @@
+const zipFolder = require('zip-folder');
+const exec      = require('child_process').exec;
+
+let folder = 'dist';
+let zipName = 'extension.zip';
+
+// credentials and IDs from gitlab-ci.yml file
+let REFRESH_TOKEN = process.env.REFRESH_TOKEN; 
+let EXTENSION_ID = process.env.EXTENSION_ID;
+let CLIENT_SECRET = process.env.CLIENT_SECRET;
+let CLIENT_ID = process.env.CLIENT_ID;
+
+// to fetch it from node_modules
+let webstoreLocation = './node_modules/.bin/webstore';
+
+zipFolder(folder, zipName, function (err) {
+  if (err) {
+    console.log('oh no!', err);
+  } else {
+    console.log(`Successfully Zipped ${folder} and saved as ${zipName}`);
+    uploadZip(); // on successful zipping, call upload 
+  }
+});
+
+function uploadZip() {
+  let cmd = getUploadCommand();
+  exec(cmd, (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    } else {
+      console.log('Successfully Uploaded the zip to chrome web store');
+      publishExtension(); // on successful upload, call publish 
+    }
+  });
+}
+
+function publishExtension() {
+  let cmd = getPublishCommand();
+  exec(cmd, (error, stdout, stderr) => {
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+    if (error !== null) {
+      console.log(`exec error: ${error}`);
+    } else {
+      console.log('Successfully published the newer version');
+    }
+  });
+}
+
+function getUploadCommand() {
+  return `${webstoreLocation} upload --source ${zipName} --extension-id ${EXTENSION_ID} --client-id ${CLIENT_ID} --client-secret ${CLIENT_SECRET} --refresh-token ${REFRESH_TOKEN}`;
+}
+
+function getPublishCommand() {
+  return `${webstoreLocation} publish --extension-id ${EXTENSION_ID} --client-id ${CLIENT_ID} --client-secret ${CLIENT_SECRET} --refresh-token ${REFRESH_TOKEN}`;
+}
